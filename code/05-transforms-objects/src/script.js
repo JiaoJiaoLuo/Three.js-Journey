547368330f7a53059afc47356fec23100a83dbc1
JiaoJiaoLuo <2447207458@qq.com>
2021-11-03 20:53:28 +0800

    Update script.js

--- code/05-transforms-objects/src/script.js
+++ code/05-transforms-objects/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { Geometry, Material } from 'three'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -16,24 +17,24 @@ scene.add(camera)
 
 
 /* Objects*/
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+ const geometry=new THREE.BoxGeometry(1, 1, 1),
+ const matetial=new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh=new THREE.Mesh(geometry,matetial)
+mesh.position.x=0.5
+mesh.position.y=-0.5
+mesh.position.z=0.7
 scene.add(mesh)
 
-//mesh.position.x=0.5;
-//mesh.position.y=-0.5;
-//mesh.position.z=0.7;
-mesh .position.set(0.4,-0.5,0.7);
 //scale//
-//mesh.scale.x=0.3
-//mesh.scale.y=0.5
-//mesh.scale.z= 0.4
+mesh.scale.x=0.3
+mesh.scale.y=0.5
+mesh.scale.z= 0.4
 console.log(mesh.position.distanceTo(camera.position))
 //rotation
 mesh.rotation.reorder('XYZ')
 mesh.rotation.x=Math.PI*0.25;
 mesh.rotation.y=Math.PI*0.25;
+
 
 
 
